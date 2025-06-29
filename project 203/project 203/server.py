817b19cae756cd47378128b51496b0be1ad0ee71
Bassam2b <164532514+Bassam2b@users.noreply.github.com>
2025-06-29 21:28:38 +0300

    Add files via upload

---
+++ project 203/project 203/server.py
@@ -0,0 +1,93 @@
+from flask import Flask, jsonify, render_template
+import sqlite3
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    return conn
+
+df = pd.read_csv("university_data.csv")
+connection = create_connection("university_data.db")
+df.to_sql('university_data', connection, if_exists='replace', index=False)
+connection.close();
+
+db_url = 'sqlite:///university_data.db'
+engine = create_engine(db_url,echo=True)
+
+df1 = pd.read_sql_query("SELECT [College] FROM university_data", engine)
+df2 = pd.read_sql_query("SELECT [Major] FROM university_data", engine)
+df3 = pd.read_sql_query("SELECT [Activity] FROM university_data", engine)
+df4 = pd.read_sql_query("SELECT [AcademicYear] FROM university_data", engine)
+
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/get-datachart")
+def get_datachart():
+    classes = df3["Activity"].value_counts().index
+    values = df3["Activity"].value_counts().values
+
+    data = []
+
+    for i in range(len(classes)):
+        data.append({"class": classes[i], "value": int(values[i])})
+
+    return jsonify(data)
+
+
+@app.route("/get-datatable")
+def get_datatable():
+    classes = df2["Major"].value_counts().index
+    values = df2["Major"].value_counts().values
+
+    datab = []
+
+    for i in range(len(classes)):
+        datab.append({"class": classes[i], "value": int(values[i])})
+
+    return jsonify(datab)
+
+
+@app.route("/get-datalol")
+def get_datalol():
+    classes = df1["College"].value_counts().index
+    values = df1["College"].value_counts().values
+
+    datac = []
+
+    for i in range(len(classes)):
+        datac.append({"class": classes[i], "value": int(values[i])})
+
+    return jsonify(datac)
+
+
+@app.route("/get-datamix2")
+def get_datamix():
+    classes = df4["AcademicYear"].value_counts().index
+    values = df4["AcademicYear"].value_counts().values
+
+    datad = []
+
+    for i in range(len(classes)):
+        datad.append({"class": classes[i], "value": int(values[i])})
+
+    return jsonify(datad)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ project 203/project 203/static/mix.js
@@ -0,0 +1,56 @@
+function fetchDataAndUpdateMix() {
+  fetch('/get-datamix2')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        updateMix(data);
+      })
+      .catch(error => console.error('Error:', error));
+}
+
+function updateMix(data_df) {
+  am5.ready(function() {
+
+      var root = am5.Root.new("mixdiv");
+
+      root.setThemes([
+          am5themes_Animated.new(root)
+      ]);
+
+      var chart = root.container.children.push(am5percent.PieChart.new(root, {
+          layout: root.verticalLayout,
+          innerRadius: am5.percent(50)
+      }));
+
+      var series = chart.series.push(am5percent.PieSeries.new(root, {
+          valueField: "value",
+          categoryField: "category",
+          alignLabels: false
+      }));
+
+      series.labels.template.setAll({
+          textType: "circular",
+          centerX: 0,
+          centerY: 0
+      });
+
+      // Update the series data with the data received from Flask
+      series.data.setAll(data_df);
+
+      // Create legend
+      var legend = chart.children.push(am5.Legend.new(root, {
+          centerX: am5.percent(50),
+          x: am5.percent(50),
+          marginTop: 15,
+          marginBottom: 15,
+      }));
+
+      legend.data.setAll(series.dataItems);
+
+      series.appear(1000, 100);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchDataAndUpdateMix();
+});
